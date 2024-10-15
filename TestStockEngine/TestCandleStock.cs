bd665ea56bd89f274d9b616f42a1a0910df5d702
OlivierVanGelder <150012161+OlivierVanGelder@users.noreply.github.com>
2024-10-15 09:32:03 +0200

    Testsproject added

---
+++ TestStockEngine/TestCandleStock.cs
@@ -0,0 +1,43 @@
+using Backend_Example.Logic.Classes;
+using Backend_Example.Logic.Stocks;
+
+namespace TestStockEngine
+{
+    [TestClass]
+    public class TestCandleStock
+    {
+        [TestMethod]
+        public void TestGetCandleValues_LowAboveHigh()
+        {
+            // Arrange
+            CandleStock candleStock = new CandleStock();
+            double mS = 20;
+            double startX = 2000;
+            double endX = 3000;
+            double interval = 1;
+
+            // Act
+            CandleItem[] values = candleStock.GetCandleValues(mS, startX, endX, interval);
+
+            // Assert
+            Assert.IsFalse(values.Any(v => v.Low > v.High));
+        }
+
+        [TestMethod]
+        public void TestGetCandleValues_VolumeNegative()
+        {
+            // Arrange
+            CandleStock candleStock = new CandleStock();
+            double mS = 20;
+            double startX = 2000;
+            double endX = 3000;
+            double interval = 1;
+
+            // Act
+            CandleItem[] values = candleStock.GetCandleValues(mS, startX, endX, interval);
+
+            // Assert
+            Assert.IsTrue(values.Any(v => v.Volume >= 0));
+        }
+    }
+}
---
+++ TestStockEngine/TestTicker.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Backend_Example.Logic.Classes;
+
+namespace TestStockEngine
+{
+    [TestClass]
+    public class TestTicker
+    {
+        [TestMethod]
+        [DataRow("ABKS", 29)]
+        [DataRow("ABAB", 2)]
+        [DataRow("DV", 24)]
+        [DataRow("", 0)]
+        [DataRow("ZZZZ", 100)]
+        public void TestConvertWordToNumber(string tickername, int expected)
+        {
+            // Arrange
+            Ticker ticker = new Ticker();
+
+            // Act
+            int result = ticker.ConvertWordToNumber(tickername);
+
+            // Assert
+            Assert.AreEqual(expected, result);
+        }
+    }
+}
