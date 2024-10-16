fa3f1a30115936266778ebac27f94cbd9f58ab6c
OlivierVanGelder <150012161+OlivierVanGelder@users.noreply.github.com>
2024-10-16 15:09:42 +0200

    Added new test

--- Backend-Example/Logic/Stocks/CandleStock.cs
+++ Backend-Example/Logic/Stocks/CandleStock.cs
@@ -4,29 +4,33 @@ namespace Backend_Example.Logic.Stocks
 {
     public class CandleStock
     {
-        public CandleItem[] GetCandleValues(double mS, double startX, double endX, double interval)
+        public static CandleItem[] GetCandleValues(
+            double mS,
+            double startX,
+            double endX,
+            double interval
+        )
         {
             // Calculate the number of values based on the range and interval
             int numberOfValues = (int)((endX - startX) / interval) + 1;
             CandleItem[] values = new CandleItem[numberOfValues];
-            Formula formula = new Formula();
 
             for (int i = 0; i < numberOfValues; i++)
             {
                 double x = startX + i * interval; // x increments by the specified interval
-                double open = formula.CalculateFormula(x, mS);
-                double close = formula.CalculateFormula(x + interval, mS);
+                double open = Formula.CalculateFormula(x, mS) ?? 0;
+                double close = Formula.CalculateFormula(x + interval, mS) ?? 0;
                 double high = open;
                 double low = open;
 
                 for (int j = 0; j < 32; j++)
                 {
-                    double temp = formula.CalculateFormula(x + 0.03125 * j * interval, mS);
+                    double temp = Formula.CalculateFormula(x + 0.03125 * j * interval, mS) ?? 0;
                     if (temp > high)
                     {
                         high = temp;
                     }
-                    else if (temp < low)
+                    else
                     {
                         low = temp;
                     }
