748e3c67d846ee3217fb0432bde6f74fe106d255
OlivierVanGelder <150012161+OlivierVanGelder@users.noreply.github.com>
2024-12-09 14:51:12 +0100

    Update login test to use specific username 'CypressTest' for improved test accuracy

--- cypress/e2e/logging-in.cy.js
+++ cypress/e2e/logging-in.cy.js
@@ -3,7 +3,7 @@ describe('Loggin in', () => {
   it('fills in the username and password, then submits the form', () => {
     cy.visit('/login'); 
 
-    cy.get('input#username').type('test');
+    cy.get('input#username').type('CypressTest');
     cy.get('input#password').type('TestingLogin');
 
     cy.get('form').submit();
