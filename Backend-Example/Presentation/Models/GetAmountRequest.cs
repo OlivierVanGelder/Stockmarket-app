608a96d43b1657ae139382312d0edeb4627ea5af
OlivierVanGelder <150012161+OlivierVanGelder@users.noreply.github.com>
2024-12-14 15:04:30 +0100

    Fixing code quality problems

--- Backend-Example/Presentation/Models/GetAmountRequest.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Backend_Example.Models
-{
-    public class GetAmountRequest
-    {
-        public string userId { get; set; } = string.Empty;
-        public string ticker { get; set; } = string.Empty;
-    }
-}
--- Backend-Example/Presentation/Models/LoginRequest.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Backend_Example.Models
-{
-    public class LoginRequest
-    {
-        public string Name { get; set; } = string.Empty;
-        public string Password { get; set; } = string.Empty;
-    }
-}
